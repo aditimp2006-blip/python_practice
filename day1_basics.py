bd890533bafbaa22284ad28d4c4ba851c69edfed
Aditi Mangesh Pharande <aditi.mp2006@gmail.com>
2026-02-01 18:07:39 +0530

    Create day1_basics.py

---
+++ day1_basics.py
@@ -0,0 +1,29 @@
+# Day 1: Python Basics Practice
+
+# 1. Print statement
+print("Hello, World!")
+
+# 2. Variables
+name = "YourName"
+age = 18
+print("Name:", name)
+print("Age:", age)
+
+# 3. Input from user
+user_name = input("Enter your name: ")
+print("Welcome,", user_name)
+
+# 4. Simple calculator
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Division:", a / b)
+
+# 5. Condition
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
